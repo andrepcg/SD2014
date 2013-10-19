51716f7c9ed6de3220864747c535fc24c0a1989f
André Perdigão <andrepcg@gmail.com>
2013-10-19 19:17:24 +0100

    funcoes do menu
    historico transaccoes, ideias

--- Projecto 1/TCPClient/ClientTCP.java
+++ Projecto 1/TCPClient/ClientTCP.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ClientTCP {
@@ -26,44 +27,54 @@ public class ClientTCP {
 		Response resposta = null;
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("1. Login\n2. Registar");
-		String input = sc.nextLine();
-
 		boolean logado = false;
+		User utilizador = null;
 
-		if (input.startsWith("2")) {
-			System.out.print("Username: ");
-			String username = sc.nextLine();
+		String input;
 
-			while (!username.matches("[a-zA-Z0-9]{4}[a-zA-Z0-9]*")) {
-				System.out.print("## minimo de 4 caracteres alfanumericos ##\nUsername: ");
-				username = sc.nextLine();
-			}
+		while (!logado) {
 
-			System.out.print("Password: ");
-			String password = sc.nextLine();
+			System.out.println("1. Login\n2. Registar");
+			input = sc.nextLine();
 
-			if (socketThread.registar(username, password))
-				System.out.println("## Registado com sucesso ##");
-		} else if (input.startsWith("1")) {
-			System.out.print("Username: ");
-			String username = sc.nextLine();
+			if (input.startsWith("2")) {
+				System.out.print("Username: ");
+				String username = sc.nextLine();
 
-			while (!username.matches("[a-zA-Z0-9]{4}[a-zA-Z0-9]*")) {
-				System.out.print("## minimo de 4 caracteres alfanumericos ##\nUsername: ");
-				username = sc.nextLine();
-			}
+				while (!username.matches("[a-zA-Z0-9]{4}[a-zA-Z0-9]*")) {
+					System.out.print("## minimo de 4 caracteres alfanumericos ##\nUsername: ");
+					username = sc.nextLine();
+				}
 
-			System.out.print("Password: ");
-			String password = sc.nextLine();
+				System.out.print("Password: ");
+				String password = sc.nextLine();
 
-			resposta = new Response();
-			socketThread.setResposta(resposta);
-			socketThread.login(username, password);
+				if (socketThread.registar(username, password))
+					System.out.println("## Registado com sucesso ##");
+			} else if (input.startsWith("1")) {
+				System.out.print("Username: ");
+				String username = sc.nextLine();
 
-			if (dados(resposta).equals("TRUE")) {
-				logado = true;
-				System.out.println("## Login com sucesso ##");
+				while (!username.matches("[a-zA-Z0-9]{4}[a-zA-Z0-9]*")) {
+					System.out.print("## minimo de 4 caracteres alfanumericos ##\nUsername: ");
+					username = sc.nextLine();
+				}
+
+				System.out.print("Password: ");
+				String password = sc.nextLine();
+
+				resposta = new Response();
+				socketThread.setResposta(resposta);
+				socketThread.login(username, password);
+
+				if (dados(resposta).contains("TRUE")) {
+					utilizador = criarUser(resposta.getResposta());
+					logado = true;
+					System.out.println("## Login com sucesso ##");
+				} else if (resposta.getResposta().contains("FALSE")) {
+					System.out.println("## Login falhado ##");
+				}
+
 			}
 
 		}
@@ -81,14 +92,43 @@ public class ClientTCP {
 				// imprimirDados(socketThread.listarTopicos(), true);
 				// Thread.currentThread().wait();
 			} else if (input.startsWith("2")) {
+				int in = 0;
+				boolean ver = false;
+				while (!ver) {
+					System.out.println("Quantidade: ");
+					try {
+						in = sc.nextInt();
+						ver = true;
+					} catch (InputMismatchException e) {
 
+					}
+				}
+
+				socketThread.adicionarPacote("HISTORICOTRANSACCOES|" + utilizador.getId() + "|" + in);
 			} else if (input.startsWith("3")) {
-
+				socketThread.adicionarPacote("IDEIAS|" + utilizador.getId());
+				imprimirDados(dados(resposta), true);
 			} else if (input.startsWith("4")) {
 
 			}
 		}
 
+	}
+
+	private void imprimirMenuPrincipal() {
+		System.out.println("\n## MENU ##");
+		System.out.println("1. Listar Topicos");
+		System.out.println("2. Historico de Transaccoes");
+		System.out.println("3. Minhas ideias");
+		System.out.println("4. Carteira de Shares");
+		// System.out.println("5. ");
+	}
+
+	private User criarUser(String dados) {
+		// LOGIN|TRUE|id|username|deicoins
+		String[] split = dados.split("\\|");
+
+		return new User(Integer.parseInt(split[2]), split[3], Double.parseDouble(split[4]));
 	}
 
 	private String dados(Response resposta) {
@@ -112,15 +152,6 @@ public class ClientTCP {
 			for (int i = 1; i < split.length; i++)
 				System.out.println((indices ? i + ". " : "") + split[i]);
 		}
-	}
-
-	private void imprimirMenuPrincipal() {
-		System.out.println("\n## MENU ##");
-		System.out.println("1. Listar Topicos");
-		System.out.println("2. Historico de Transaccoes");
-		System.out.println("3. Minhas ideias");
-		System.out.println("4. Carteira de Shares");
-		// System.out.println("5. ");
 	}
 
 	private static String getHost(String args[]) {
